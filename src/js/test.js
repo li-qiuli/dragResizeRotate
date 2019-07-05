9b01f95690ea62d0d6c9beed06f958abc434e20a
epubliqiuli <1914394696@qq.com>
2019-07-05 09:56:09 +0800

    feat: 🎸 mobx监测数据变化

--- src/js/test.js
+++ src/js/test.js
@@ -1,17 +1,8 @@
-
-import { decorate, observable } from "mobx";
-// dom 实例 缓存
-// transform
-// render function 手动的
-
-// observerable data =>  event
-// alloyfinger 
-
-// new Allouy({
-//     el:''
-//     onSTart,
-// })
-import $ from 'jquery'
+//******（已封装 已用mobx） */
+import {
+    observable,
+    autorun
+} from "mobx";
 const {
     modeChoice: md,
     markerChoice: mk,
@@ -19,60 +10,205 @@ const {
     resize,
     rotate
 } = require('drag-resize-rotate');
+import $ from 'jquery'
 
 //* 缩放
 let arrResize = [{
     name: '.drr-stick-tl',
     direction: mk.leftTop,
-    classNames:'drr-stick drr-stick-tl'
+    classNames: 'drr-stick drr-stick-tl'
 }, {
     name: '.drr-stick-tr',
     direction: mk.rightTop,
-    classNames:'drr-stick drr-stick-tr'
+    classNames: 'drr-stick drr-stick-tr'
 }, {
     name: '.drr-stick-bl',
     direction: mk.leftBottom,
-    classNames:'drr-stick drr-stick-bl'
+    classNames: 'drr-stick drr-stick-bl'
 }, {
     name: '.drr-stick-br',
     direction: mk.rightBottom,
-    classNames:'drr-stick drr-stick-br'
+    classNames: 'drr-stick drr-stick-br'
 }]
 
-/**
- * *record rotate
- */
+
+
+
+//初始center
+function centerfun() {
+    let c = document.getElementsByClassName('drag')[0].style.transform.match(/\d+(.\d+)?/g)
+    let centerX = parseFloat(c[c.length - 2]) + $('.drag').outerWidth() / 2
+    let centerY = parseFloat(c[c.length - 1]) + $('.drag').outerHeight() / 2
+    let center = [centerX, centerY]
+    return center
+}
+
+//初始center
+function centerfun1() {
+    let c = document.getElementsByClassName('drag')[0].style.transform.match(/\d+(.\d+)?/g)
+    let centerX = parseFloat(c[c.length - 2])
+    let centerY = parseFloat(c[c.length - 1])
+    let center = [centerX, centerY]
+    return center
+}
+
+var initCenter = centerfun()
+let initSize = [$('.drag').outerWidth(), $('.drag').outerHeight()]
 let commonRotate = 0
 
+let numberValue = observable({
+    el: $('.drag'),
+    size: initSize,
+    center: initCenter,
+    rotate: commonRotate,
+})
 
-//TODO  new DRR(O1,rotate)                 //* 旋转  ！！！！div
-//TODO  new DRR(R1,R2,R3,R4,Resize)        //* 缩放  ！！！！div
-//TODO  new DRR(div,drag)                  //* 拖拽
-//TODO   var DRR = function (el, option) {} 
- 
-/**
- * *get center
- */
-const conmmonCenter = () => {
-    let x = $('.drag').offset().left + $('.drag').outerWidth() / 2;
-    let y = $('.drag').offset().top + $('.drag').outerHeight() / 2
-    let touchCenter = [x, y]
-    return touchCenter
+//值改变时调用  渲染DOM
+autorun(
+    function operateDOMRender() {
+        const {
+            el: el,
+            rotate: rx,
+            size: [sx, sy],
+            center: [cx, cy],
+        } = numberValue;
+        let cssName = {
+            transform: `translate(-50%, -50%) translate(${cx}px,${cy}px) rotate(${rx}deg)`,
+            width: `${sx}px`,
+            height: `${sy}px`,
+            transformOrigin: `center` //center
+        }
+        el.css(cssName)
+    }
+)
+
+//*拖拽  ======== 改变{center:[110, 240]}
+const pcDrag = (dom1, dom2) => {
+    dom1.mousedown(function (event) {
+        let center = initCenter
+        let initsize = [$('.drag').outerWidth(), $('.drag').outerHeight()]
+        event.stopPropagation();
+        let startPointT = [event.clientX, event.clientY]
+        dom2.mousemove(function (ev) {
+
+            ev.stopPropagation();
+            ev.preventDefault()
+            const opt1 = {
+                startPos: {
+                    center: center
+                },
+                opts: {
+                    startPoint: startPointT,
+                    movePoint: [ev.clientX, ev.clientY],
+                },
+            };
+
+            const result = move(opt1)
+
+            numberValue.center = result.center
+            // numberValue.translate = [result.center[0] - initsize[0] / 2, result.center[1] - initsize[1] / 2]
+            initCenter = result.center
+
+        })
+    })
+    dom1.mouseup(function (ev) {
+        ev.stopPropagation();
+        dom1.unbind("mousemove")
+        dom2.unbind("mousemove")
+    })
+}
+
+//*缩放 ===========改变 { size: [ 58.57864376269052, 29.28932188134526 ], center: [ 25, 25 ] }
+const pcResize = (arrResize, dom2, dom3) => {
+
+    for (let i = 0; i < arrResize.length; i++) {
+        $(arrResize[i].name).mousedown(function (event) {
+            event.stopPropagation();
+            let startPointT = [event.clientX, event.clientY]
+            let center = initCenter
+            const startPos = {
+                center: center,
+                rotate: commonRotate,
+                size: initSize
+            }
+            dom3.mousemove(function (eve) {
+                eve.stopPropagation();
+                let movePoint = [eve.clientX, eve.clientY]
+                const opt3 = {
+                    startPos,
+                    opts: {
+                        startPoint: startPointT,
+                        movePoint: movePoint,
+                        mode: md.ratio,
+                        marker: arrResize[i].direction
+                    },
+                };
+                const result = resize(opt3) //====>center   size
+                console.log(3333333, result)
+                Object.assign(numberValue,result)
+                initSize = result.size
+                initCenter = result.center
+            })
+        })
+    }
+    for (let i = 0; i < arrResize.length; i++) {
+        $(arrResize[i].name).mouseup(function (event) {
+            dom3.unbind("mousemove")
+        })
+        dom3.mouseup(function (event) {
+            event.stopPropagation();
+            dom3.unbind("mousemove")
+        })
+    }
+}
+
+//*旋转 ========== 改变 { rotate: -60 }
+const pcRotate = (dom1, dom2, dom3) => {
+    dom1.mousedown(function (event) {
+        event.stopPropagation();
+        let center = initCenter
+
+        let startPos = {
+            center: center,
+            rotate: commonRotate
+        }
+        let startPointT = [event.clientX, event.clientY]
+        dom2.mousemove(function (ev) {
+            let movePoint = [ev.clientX, ev.clientY]
+            ev.stopPropagation();
+            const opt2 = {
+                startPos,
+                opts: {
+                    startPoint: startPointT,
+                    movePoint,
+                },
+            };
+            let result = rotate(opt2)
+            numberValue.rotate = result.rotate
+            commonRotate = result.rotate
+        })
+    })
+    dom1.mouseup(function (event) {
+        event.stopPropagation();
+        dom2.unbind("mousemove")
+    })
 }
 
 
 
 
-//*拖拽（移动）
-const epubDrag = (dom,classNames) => {
+//*拖拽（移动）======== 改变{center:[110, 240]}
+const epubDrag = (dom, classNames) => {
     let startPointT = []
     let touchCenter = []
+    let initsize = [$('.drag').outerWidth(), $('.drag').outerHeight()]
     dom[0].addEventListener('touchstart', function (event) {
-    // $('.drag')[0].addEventListener('touchstart', function (event) {
         //开始点
+
         event.stopPropagation();
-        touchCenter = conmmonCenter()
+        touchCenter = initCenter
         startPointT = [event.changedTouches[0].clientX, event.changedTouches[0].clientY]
+
         if (event.target.className == classNames) {
             dom[0].addEventListener('touchmove', function (ev) {
                 ev.stopPropagation();
@@ -87,32 +223,31 @@ const epubDrag = (dom,classNames) => {
                         },
                     };
                     const result = move(opt1)
-                    operateDOMRender(dom, result)
+                   
+                    numberValue.center = result.center
+                    initCenter = result.center
                 }
             })
         }
     })
-    // $('.drag')[0].addEventListener('touchend', function (event) {
-    //     $('.drag')[0].addEventListener('touchstart', function () {})
-    //     $('.drag')[0].addEventListener('touchmove', function () {})
-    // })
 }
 
-//*缩放（移动）
-const epubResize = (arrResize,dom2) => {
+//*缩放（移动）===========改变 { size: [ 58.57864376269052, 29.28932188134526 ], center: [ 25, 25 ] }
+const epubResize = (arrResize, dom2) => {
     let touchCenter = []
     let startPointT = []
     for (let i = 0; i < arrResize.length; i++) {
         $(arrResize[i].name)[0].addEventListener('touchstart', function (event) {
             event.preventDefault()
             event.stopPropagation();
-            touchCenter = conmmonCenter()
+            touchCenter = initCenter
             let startPosT = {
                 center: touchCenter,
                 rotate: commonRotate,
-                size: [dom2.outerWidth(), dom2.outerHeight()]
+                size: initSize
             }
             startPointT = [event.changedTouches[0].clientX, event.changedTouches[0].clientY]
+
             if (event.target.className == arrResize[i].classNames) {
                 $(arrResize[i].name)[0].addEventListener('touchmove', function (ev) {
                     const opt3 = {
@@ -124,20 +259,25 @@ const epubResize = (arrResize,dom2) => {
                             marker: arrResize[i].direction
                         },
                     };
-                    const result = resize(opt3)
-                    operateDOMRender(dom2, result)
+                   
+                    const result = resize(opt3) //====>center   size
+                    numberValue.center = result.center
+                    numberValue.size = result.size
+                    
+                    initSize = result.size
+                    initCenter = result.center
                 })
             }
         })
     }
 }
 
-//*旋转(移动)
-const epubRotate = (dom1,dom2,classNames) => {
+//*旋转(移动) ========== 改变 { rotate: -60 }
+const epubRotate = (dom1, dom2, classNames) => {
     dom1[0].addEventListener('touchstart', function (event) {
         event.stopPropagation();
         let startPointT = []
-        let touchCenter = conmmonCenter()
+        let touchCenter = initCenter
 
         let startPosR = {
             center: touchCenter,
@@ -155,164 +295,25 @@ const epubRotate = (dom1,dom2,classNames) => {
                     },
                 };
                 const result = rotate(opt2)
-                result['center'] = touchCenter
+                numberValue.rotate = result.rotate
                 commonRotate = result.rotate
-                operateDOMRender(dom2, result)
+              
             })
         }
     })
 }
 
 
-// TODO drag-calc src
-
-//*拖拽
-const pcDrag = (dom1,dom2) => {
-    dom1.mousedown(function (event) {
-        let center = operateCenter(dom1)
-
-        event.stopPropagation();
-        let startPointT = [event.clientX,event.clientY]
-
-        dom2.mousemove(function (ev) {
-        // dom2.mousemove(function (ev) {
-            ev.stopPropagation();
-            ev.preventDefault()
-            const opt1 = {
-                startPos: {
-                    center: center
-                },
-                opts: {
-                    startPoint: startPointT,
-                    movePoint: [ev.clientX, ev.clientY],
-                },
-            };
-            operateDOMRender(dom1, move(opt1))
-        })
-    })
-    dom1.mouseup(function (ev) {
-        ev.stopPropagation();
-        dom1.unbind("mousemove")
-        dom2.unbind("mousemove")
-    })
-}
-
-//*缩放
-const pcResize = (arrResize,dom2,dom3) => {
-    for (let i = 0; i < arrResize.length; i++) {
-        $(arrResize[i].name).mousedown(function (event) {
-            event.stopPropagation();
-
-            let startPointT = [event.clientX, event.clientY]
-            let center = operateCenter(dom2)
-
-            const startPos = {
-                center: center,
-                rotate: commonRotate,
-                size: [dom2.outerWidth(), dom2.outerHeight()]
-            }
-
-            dom3.mousemove(function (event) {
-                event.stopPropagation();
-                let movePoint = [event.clientX, event.clientY]
-                const opt3 = {
-                    startPos,
-                    opts: {
-                        startPoint: startPointT,
-                        movePoint: movePoint,
-                        mode: md.ratio,
-                        marker: arrResize[i].direction
-                    },
-                };
-                operateDOMRender(dom2, resize(opt3))
-            })
-        })
-    }
-    for (let i = 0; i < arrResize.length; i++) {
-        $(arrResize[i].name).mouseup(function (event) {
-            dom3.unbind("mousemove")
-        })
-        dom3.mouseup(function (event) {
-            event.stopPropagation();
-            dom3.unbind("mousemove")
-        })
-    }
-}
-
-//*旋转
-const pcRotate = (dom1,dom2,dom3) => {
-    // //*旋转
-    dom1.mousedown(function (event) {
-        event.stopPropagation();
-        let center = operateCenter(dom3)
-
-        let startPos = {
-            center: center,
-            rotate: commonRotate
-        }
-        let startPointT = [event.clientX, event.clientY]
-        dom2.mousemove(function (ev) {
-            let movePoint = [ev.clientX, ev.clientY]
-            ev.stopPropagation();
-            const opt2 = {
-                startPos,
-                opts: {
-                    startPoint:startPointT,
-                    movePoint,
-                },
-            };
-            let result = rotate(opt2)
-            result['center'] = center
-            operateDOMRender(dom3, result)
-            commonRotate = rotate(opt2).rotate
-        })
-    })
-    dom1.mouseup(function (event) {
-        event.stopPropagation();
-        dom2.unbind("mousemove")
-    })
-
-}
-
-const operateCenter = (dom) => {
-    let x = dom.offset().left + dom.outerWidth() / 2
-    let y = dom.offset().top + dom.outerHeight() / 2
-    let center = [x, y]
-    return center
-}
-
-const operateDOMRender = (DOM, value) => {
-    let param = {
-        size: [DOM.outerWidth(), DOM.outerHeight()],
-        center: [DOM.offset().left + DOM.outerWidth() / 2, DOM.offset().top + DOM.outerHeight() / 2],
-        rotate: commonRotate
-    };
-    let result = Object.assign(param, value);
-    const {
-        size: [sx, sy],
-        center: [cx, cy],
-        rotate: rx,
-    } = result;
-
-    let cssName = {
-        transform: `rotate(${rx}deg) translate(${cx-DOM.outerWidth()/2}px,${cy-DOM.outerHeight()/2}px)`,
-        width: `${sx}px`,
-        height: `${sy}px`,
-        transformOrigin: `${cx}px ${cy}px`
-    }
-    DOM.css(cssName)
-}
-
-let c=navigator.userAgent
+let c = navigator.userAgent
 if (/Macintosh/.test(c)) {
     pcDrag($('.drag'), $('#root-wrapper'))
-    pcResize(arrResize,$('.drag'),$('#root-wrapper'))
-    pcRotate($('.drr-stick-ro'),$('#root-wrapper'),$('.drag'))
+    pcResize(arrResize, $('.drag'), $('#root-wrapper'))
+    pcRotate($('.drr-stick-ro'), $('#root-wrapper'), $('.drag'))
 } else {
     //拖拽
-    epubDrag($('.drag'),'dragPic image')
+    epubDrag($('.drag'), 'dragPic image')
     // 缩放
-    epubResize(arrResize,$('.drag'),$('#root-wrapper'))
+    epubResize(arrResize, $('.drag'), $('#root-wrapper'))
     //旋转
-    epubRotate($('.drr-stick-ro'),$('.drag'),'drr-stick drr-stick-ro')
+    epubRotate($('.drr-stick-ro'), $('.drag'), 'drr-stick drr-stick-ro')
 }
